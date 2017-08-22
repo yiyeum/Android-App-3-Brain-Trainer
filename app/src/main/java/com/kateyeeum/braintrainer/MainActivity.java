e878a3bcd321b22a0daaaa22757ad708335b58f1
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-08-22 13:55:09 -0700

    Game Layout Design completed.
    -timerTextView
    -sumTextView
    -scoreTextView
    -resultTextView
    -gridLayout
       -button0, button1, button2, button3

--- app/src/main/java/com/kateyeeum/braintrainer/MainActivity.java
+++ app/src/main/java/com/kateyeeum/braintrainer/MainActivity.java
@@ -2,12 +2,27 @@ package com.kateyeeum.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    Button goButton;
+    RelativeLayout gameLayout;
+
+    public void start(View view){
+        goButton.setVisibility(View.INVISIBLE);
+        gameLayout.setVisibility(View.VISIBLE);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goButton = (Button) findViewById(R.id.goButton);
+        gameLayout = (RelativeLayout) findViewById(R.id.gameLayout);
     }
 }
